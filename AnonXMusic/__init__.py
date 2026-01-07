8cd9ce73e639d0431c18963342c32f8f5168fe39
sonughs <50259651+sonughs@users.noreply.github.com>
2026-01-08 01:13:15 +0530

    Update __init__.py

--- AnonXMusic/__init__.py
+++ AnonXMusic/__init__.py
@@ -1,19 +1,22 @@
+import os
+
 from AnonXMusic.core.bot import Anony
 from AnonXMusic.core.dir import dirr
 from AnonXMusic.core.git import git
 from AnonXMusic.core.userbot import Userbot
 from AnonXMusic.misc import dbb, heroku
-
 from .logging import LOGGER
 
 dirr()
-git()
+
+if not os.getenv("DOCKER"):
+      git()
+
 dbb()
 heroku()
 
 app = Anony()
 userbot = Userbot()
-
 
 from .platforms import *
 
